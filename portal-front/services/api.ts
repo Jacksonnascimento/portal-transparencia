ed72a5817ff1d9d77603701af214f8772e37bfae
Jackson Nascimento <44805133+Jacksonnascimento@users.noreply.github.com>
2026-03-09 04:03:29 -0300

    add

--- portal-front/services/api.ts
+++ portal-front/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // Se não houver variável de ambiente, o padrão é o localhost
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api/v1',
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL || ''}/api/v1`,
 });
 
 export default api;
